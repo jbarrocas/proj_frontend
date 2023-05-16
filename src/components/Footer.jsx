032aa078a5fd3d09235ef51be23ccd56e4da3f81
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-05-16 13:05:32 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from './Button';
+
+import Logo from '../assets/Finsweet-Logo.png';
+import FacebookIcon from '../assets/social_media_icons/Facebook.svg';
+import TwitterIcon from '../assets/social_media_icons/Twitter.svg';
+import InstagramIcon from '../assets/social_media_icons/Instagram.svg';
+import LinkedinIcon from '../assets/social_media_icons/LinkedIn.svg';
+
+import './Footer.scss';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <nav className="footer-navbar">
+        <Link to="/"><img className="logo" src={Logo} alt="Logo"></img></Link>
+        <ul className="footer-menu">
+          <li className="menu-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/Blog">Blog</Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/Contact">Contact us</Link>
+          </li>
+          <li className="menu-item">
+            <Link to="#">Privacy Policy</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="subscribe-section">
+        <h2 className="sub-heading">Subscribe to our newsletter to get latest updates and news</h2>
+        <div className="sub-input">
+          <input className="mail-input" type='mail' placeholder='Enter Your Email'></input>
+          <Button className="yellow-button" text="Subscribe" />
+        </div>
+      </div>
+      <div className="address-social">
+        <address className="address">
+          <p>Finstreet 118 2561 Fintown</p>
+          <p>Hello@finsweet.com  020 7993 2905</p>
+        </address>
+        <div className="social-media">
+        <img className="facebook-icon" src={FacebookIcon} alt="Facebook Icon"></img>
+        <img className="twitter-icon" src={TwitterIcon} alt="Twitter Icon"></img>
+        <img className="instagram-icon" src={InstagramIcon} alt="Instagram Icon"></img>
+        <img className="linkedin-icon" src={LinkedinIcon} alt="LinkedIn Icon"></img>
+        </div>
+      </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,45 +11,47 @@ function Navbar() {
     const [isMenuExpanded, setIsMenuExpanded] = useState(false);
 
     return(
-        <nav className="navbar">
-            <Link to="/"><img className="logo" src={Logo} alt="Logo"></img></Link>
-            <ul className={
-            isMenuExpanded ? "menu-expanded menu" : "menu"
-            }>
-                <li className="menu-item" onClick={()=>{
-                setIsMenuExpanded(!isMenuExpanded)
-                }}>
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="menu-item" onClick={()=>{
-                setIsMenuExpanded(!isMenuExpanded)
-                }}>
-                    <Link to="/Blog">Blog</Link>
-                </li>
-                <li className="menu-item" onClick={()=>{
-                setIsMenuExpanded(!isMenuExpanded)
-                }}>
-                    <Link to="/Contact">Contact us</Link>
-                </li>
-                <li className="menu-item">
-                    <Button className="white-button" text="Subscribe"/>
-                </li>
-                <button className="menu-close-btn" onClick={()=>{
-                setIsMenuExpanded(!isMenuExpanded)
+        <header>
+            <nav className="navbar">
+                <Link to="/"><img className="logo" src={Logo} alt="Logo"></img></Link>
+                <ul className={
+                isMenuExpanded ? "menu-expanded menu" : "menu"
+                }>
+                    <li className="menu-item" onClick={()=>{
+                    setIsMenuExpanded(!isMenuExpanded)
+                    }}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="menu-item" onClick={()=>{
+                    setIsMenuExpanded(!isMenuExpanded)
+                    }}>
+                        <Link to="/Blog">Blog</Link>
+                    </li>
+                    <li className="menu-item" onClick={()=>{
+                    setIsMenuExpanded(!isMenuExpanded)
+                    }}>
+                        <Link to="/Contact">Contact us</Link>
+                    </li>
+                    <li className="menu-item">
+                        <Button className="white-button" text="Subscribe"/>
+                    </li>
+                    <button className="menu-close-btn" onClick={()=>{
+                    setIsMenuExpanded(!isMenuExpanded)
+                    }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                    </button>
+                </ul>
+                <button className="menu-btn" onClick={()=>{
+                    setIsMenuExpanded(!isMenuExpanded)
                 }}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
                 </svg>
                 </button>
-            </ul>
-            <button className="menu-btn" onClick={()=>{
-                setIsMenuExpanded(!isMenuExpanded)
-            }}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
-            </svg>
-            </button>
-        </nav>
+            </nav>
+        </header>
     )
 }
 
