510379658cee71e09a751335a1b90e611b0e8851
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-05-29 12:05:43 +0100

    footer finished w/ active buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import Logo from '../assets/Finsweet-Logo.png';
 
 import '../styles/components/Footer.scss';
 
 function Footer() {
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split('/');
+  
   return (
-    <footer>
+    <footer className='footer'>
       <div className='footer-navbar' id='footer'>
         <Link to='/'><img className='logo' src={Logo} alt='Logo'></img></Link>
         <ul className='footer-menu'>
-          <li className='menu-item'>
-            <Link to='/'>Home</Link>
-          </li>
-          <li className='menu-item'>
-            <Link to='/blog'>Blog</Link>
-          </li>
-          <li className='menu-item'>
-            <Link to='/about'>About Us</Link>
-          </li>
-          <li className='menu-item'>
-            <Link to='/contact'>Contact us</Link>
-          </li>
-          <li className='menu-item'>
-            <Link to='/privacy'>Privacy Policy</Link>
-          </li>
+          <Link to='/'>
+            <li className={splitLocation[1] === '' ? 'menu-item active' : 'menu-item'}>Home
+            </li>
+          </Link>
+          <Link to='/blog'>
+            <li className={splitLocation[1] === 'blog' ? 'menu-item active' : 'menu-item'}>Blog
+            </li>
+          </Link>
+          <Link to='/about'>
+            <li className={splitLocation[1] === 'about' ? 'menu-item active' : 'menu-item'}>About Us
+            </li>
+          </Link>
+          <Link to='/contact'>
+            <li className={splitLocation[1] === 'contact' ? 'menu-item active' : 'menu-item'}>Contact us
+            </li>
+          </Link>
+          <Link to='/privacy'>
+            <li className={splitLocation[1] === 'privacy' ? 'menu-item active' : 'menu-item'}>Privacy Policy
+            </li>
+          </Link>
         </ul>
       </div>
       <div className='subscribe-section'>
         <h2 className='subscribe-heading'>Subscribe to our newsletter to get latest updates and news</h2>
         <div className='subscribe-input'>
           <input className='mail-input' type='mail' placeholder='Enter Your Email'></input>
-          <div className='yellow-button'>
-            <Link to='/#'>Subscribe</Link>
-          </div>
+          <Link to=''>
+            <div className='yellow-button'>Subscribe
+            </div>
+          </Link>
         </div>
       </div>
       <div className='address-social'>
@@ -43,10 +53,10 @@ function Footer() {
           <p>Hello@finsweet.com  020 7993 2905</p>
         </address>
         <div className='social-media'>
-          <Link to='/#'><img className='facebook-icon' src='../assets/social_media_icons/Facebook.svg' alt='Facebook Icon'></img></Link>
-          <Link to='/#'><img className='twitter-icon' src='../assets/social_media_icons/Twitter.svg' alt='Twitter Icon'></img></Link>
-          <Link to='/#'><img className='instagram-icon' src='../assets/social_media_icons/Instagram.svg' alt='Instagram Icon'></img></Link>
-          <Link to='/#'><img className='linkedin-icon' src='../assets/social_media_icons/Linkedin.svg' alt='LinkedIn Icon'></img></Link>
+          <Link to='https://facebook.com/' target='blank'><img className='facebook-icon' src='../assets/social_media_icons/Facebook.svg' alt='Facebook Icon'></img></Link>
+          <Link to='https://twitter.com/' target='blank'><img className='twitter-icon' src='../assets/social_media_icons/Twitter.svg' alt='Twitter Icon'></img></Link>
+          <Link to='https://instagram.com/' target='blank'><img className='instagram-icon' src='../assets/social_media_icons/Instagram.svg' alt='Instagram Icon'></img></Link>
+          <Link to='https://linkedin.com/' target='blank'><img className='linkedin-icon' src='../assets/social_media_icons/Linkedin.svg' alt='LinkedIn Icon'></img></Link>
         </div>
       </div>
     </footer>
