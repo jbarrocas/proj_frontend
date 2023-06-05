294abdc3d707bad5ab22360d261186819683b039
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-06-05 18:32:35 +0100

    Testimonials w/ buttons out of render

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -25,31 +25,28 @@ const Testimonials = ({ data }) => {
                 return (
                     <div key={id} className={ testimonial === id ? 'testimonials-carousel' : 'testimonials-carousel hidden'}>
                         <h4 className='testimonial'>{item.testimonial}</h4>
-                        <div className='avatar-arrows'>
-                            <div className='avatar-wrapper'>
-                                <img className='avatar' src={item.image} alt="" />
-                                <div className='identity'>
-                                    <h4 className='name'>{item.name}</h4>
-                                    <p className='location'>{item.city}, {item.country}</p>
-                                </div>
-                            </div>
-
-                            <div className='arrows'>
-                                <div className='left-arrow' onClick={prevTestimonial}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                </div>
-                                <div className='right-arrow' onClick={nextTestimonial}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                </div>
+                        <div className='avatar-wrapper'>
+                            <img className='avatar' src={item.image} alt="" />
+                            <div className='identity'>
+                                <h4 className='name'>{item.name}</h4>
+                                <p className='location'>{item.city}, {item.country}</p>
                             </div>
                         </div>
                     </div>
                 )
             })}
+            <div className='arrows'>
+                <div className='left-arrow' onClick={prevTestimonial}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+                <div className='right-arrow' onClick={nextTestimonial}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+            </div>
 
         </div>
     )
